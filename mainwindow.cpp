0c559b87777356814000660e54887154db8c1ed7
Анна Сунцова <sas_kuz2000@mail.ru>
2021-05-06 12:29:45 +0300

    update qt window

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFileDialog>
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
@@ -11,4 +12,20 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_pushButton_input_file_clicked()
+{
+    QString path = QFileDialog::getOpenFileName(0,QObject::tr("Укажите файл c входным текстом"),QDir::homePath(), QObject::tr("Текстовый файл (*.txt);;Все файлы (*.*)"));
+    if (!path.isEmpty()) {
+        this->ui->lineEdit_input->setText(path);
+    }
+}
+
+void MainWindow::on_pushButton_output_clicked()
+{
+    QString path = QFileDialog::getOpenFileName(0,QObject::tr("Укажите файл для вывода результата"),QDir::homePath(), QObject::tr("Текстовый файл (*.txt);;Все файлы (*.*)"));
+    if (!path.isEmpty()) {
+        this->ui->lineEdit_output->setText(path);
+    }
 }
